8b43834eda99d9f7e1a0f62f4c101a6ce4a3085e
Lea SAGNIER <lea.sagnier@vinci-energies.net>
2022-05-29 17:14:12 +0200

    cookies

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,41 @@ function hoverModeleF(){
 function hoverModeleFlipper(){
   document.getElementById('design-img-block').style.backgroundImage='url(/assets/img/modele_flipper.png)';
 }
+// Show cookies pop up
+function showCookiesBock(){
+  document.getElementById('messagecookies').classList.remove('d-none')
+}
+// Cookie Law 
+
+function setCookie(cookieName, cookieValue, numdaystilexpireasinteger) {
+  var d = new Date();
+  d.setTime(d.getTime() + (numdaystilexpireasinteger*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cookieName+ "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+function getCookie(cookieName) {
+  var name = cookieName+ "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
+}
+
+function showLaw() {
+  var x = getCookie("cookieName");  //call cookie to get its value
+  if (x != "") {
+      $("#messagecookies").remove();
+  } else {
+          setCookie("cookieName", "cookieValue", 2);
+      }
+  }
+
